cc6bd4d0e51bc230c0bb721a5bd6521fc0a041d9
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-08-22 16:44:20 -0700

    played with bundle stuff added some squares working but still slow with event listeners

--- setup/index.js
+++ setup/index.js
@@ -2,13 +2,16 @@ var $ = require('jquery')
 
 console.log("reaching line 3!")
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" )
 
-    $(".thorax1").click(function(){
-      // e.preventDefault()
+      $(".thorax1").click(function(){
+      //e.preventDefault()
       console.log("thorax1 was clicked!")
     })
+
+
+
 
 
 })
